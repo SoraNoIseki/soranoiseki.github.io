0488d180e0827d00c2f27b74874ca9c61179157d
SoraNoIseki <qyz1206@hotmail.com>
2024-11-27 16:15:24 +0100

    feat: mobile nav menu

--- themes/skynova/assets/js/main.js
+++ themes/skynova/assets/js/main.js
@@ -1,1 +1,17 @@
 import 'flowbite';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Select the toggle button and sidebar
+    const toggleButton = document.getElementById('menu-toggle');
+    const closeButton = document.getElementById('menu-close');
+    const sidebar = document.getElementById('sidebar');
+
+    // Add click event listener
+    toggleButton?.addEventListener('click', () => {
+        sidebar.classList.toggle('translate-x-full');
+    });
+
+    closeButton?.addEventListener('click', () => {
+        sidebar.classList.toggle('translate-x-full');
+    });
+});
